bf2522105a2769ba20538157f4ae2a318423e350
Ulises Baroni <ulisesbaroni@gmail.com>
2026-08-10 17:15:47 -0300

    Modelo de Usuario con bcrypt instalado

---
+++ src/models/user.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    age: { type: Number, required: true },
+    password: { type: String, required: true },
+    cart: { type: mongoose.Schema.Types.ObjectId, ref: "Cart" },
+    role: { type: String, default: "user" },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      transform: (_doc, ret) => {
+        delete ret.password;
+        return ret;
+      },
+    },
+  }
+);
+
+export default mongoose.model("User", userSchema);
